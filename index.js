dd74fc0a950b02854bd9cbe345a80bd877df392b
Kevin Cameron <kevinacameron@hotmail.com>
2018-02-12 11:05:20 -0800

    Set styles

--- index.js
+++ index.js
@@ -1,30 +1,51 @@
 "use strict";
 
+const styles = [ "s0", "s1", "s2" ];
+
+function addBr(text) {
+    return text.replace(/\n/g, "<br>")
+}
+
 m.mount(document.getElementById("mount"), {
     oninit : (vnode) => {
         vnode.state.lyrics = lyrics || [];
     },
-    view : (vnode) =>
-        lyrics
-        .map((lineObj) =>
-            Object.assign(lineObj, {
-                text : lineObj.text.replace(/\n/g, "<br>")
-            })
-        )
-        .map((lineObj, idx) =>
-            m("p", {
-                    class : [
-                            vnode.state.selected === idx ? "selected" : "",
-                            "line",
-                            lineObj.hash
-                        ].join(" "),
+    view : (vnode) => [
+        m("div", { class : "lyrics" },
+            lyrics
+            .map((part, idx) =>
+                m("p", {
+                        class : [
+                                vnode.state.selected === idx ? "selected" : "",
+                                "line",
+                                part.hash,
+                                part.class || ""
+                            ].join(" "),
+
+                        onclick : () => {
+                            vnode.state.selected = vnode.state.selected === idx ? false : idx;
+                        }
+                    },
+
+                    m.trust(addBr(part.text))
+                )
+            )
+        ),
+
+        m("div", { class : "styles" },
+            styles.map((style, idx) =>
+                m("button", {
+                    class : style,
 
                     onclick : () => {
-                        vnode.state.selected = vnode.state.selected === idx ? false : idx;
-                    }
-                },
+                        if(isNaN(vnode.state.selected)) {
+                            return;
+                        }
 
-                m.trust(lineObj.text)
+                        vnode.state.lyrics[vnode.state.selected].class = style;
+                    }
+                }, `Style ${idx}`)
             )
         )
+    ]
 });
