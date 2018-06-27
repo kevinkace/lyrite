f6a17874341e48f2d297dcc6630e5ee7e99b1275
Kevin Cameron <kevinacameron@hotmail.com>
2018-06-26 22:16:15 -0700

    FB auth

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -10,12 +10,22 @@ const modals = { login };
 const hasClose = [ "login" ];
 
 export default {
-    // onbeforeremove : (vnode) => Promise.all([
-    //     animResolve(vnode.dom, css.modalOut),
-    //     animResolve(vnode.state.contentVnode.dom, css.contentOut)
-    // ]),
+    onbeforeremove : (vnode) =>
+        Promise.all([
+            animResolve(vnode.dom, css.modalOut),
+            animResolve(vnode.state.contentVnode.dom, css.contentOut)
+        ]),
     view : (vnode) =>
-        m("div", { class : css.modalIn },
+        m("div", {
+                onclick : (e) => {
+                    if(e.target !== e.currentTarget) {
+                        return;
+                    }
+
+                    state.action("CLOSE MODAL");
+                },
+                class   : css.modalIn
+            },
             m("div", {
                     class    : css.contentIn,
                     oncreate : (contentVnode) => {
--- src/components/modal/login.js
+++ src/components/modal/login.js
@@ -1,9 +1,28 @@
 import m from "mithril";
 
+import state from "../../state";
+
+import { firebase } from "../../db";
+
 
 export default {
-    view : (vnode) =>
-        m("a", {
-            href : "#"
-        }, "twitter")
+    view() {
+        return m("button", {
+            onclick : () => {
+                const provider = new firebase.auth.TwitterAuthProvider();
+
+                firebase.auth().signInWithPopup(provider)
+                    .then((result) => {
+                        const token = result.credential.accessToken;
+                        const secret = result.credential.secret;
+                        const user = result.user;
+                        debugger;
+                    })
+                    .catch((err) => {
+                        debugger;
+                        console.log(err);
+                    });
+            }
+        }, "twitter");
+    }
 };
