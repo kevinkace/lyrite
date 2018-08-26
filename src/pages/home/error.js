d46588fc45237fc658c5585f9eb88a7791c99e4a
Kevin Cameron <kevinacameron@hotmail.com>
2018-08-25 22:55:03 -0700

    Better error

--- src/pages/home/error.js
+++ src/pages/home/error.js
@@ -1,11 +1,19 @@
 import m from "mithril";
 
 import css from "./error.css";
+import resolveAnimation from "../../lib/animResolve";
 
 export default {
     view(vnode) {
         const va = vnode.attrs;
 
-        return va.show && va.errors ? m("div", { class : css.error }, va.errors.map((error) => error.label)) : null;
+        return va.show && va.errors ?
+            m("div", {
+                    onbeforeremove : (vnode) => resolveAnimation(vnode.dom, css.errorOut),
+                    class : css.errorIn
+                },
+                va.errors.map((error) => error.label)
+            ) :
+            null;
     }
-}
+};
