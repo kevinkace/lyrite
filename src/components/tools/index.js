79debadff77f665ede5cc2eac75481e7465f1b4c
kevinkace <kevinacameron@hotmail.com>
2018-11-24 16:18:22 -0800

    Col icons

--- src/components/tools/index.js
+++ src/components/tools/index.js
@@ -115,7 +115,7 @@ export default {
 
                             --state.cols.count;
                         }
-                    }, "<"),
+                    }, "-"),
 
                     m("label", { class : css.cols }, state.cols.count),
 
@@ -124,7 +124,7 @@ export default {
                         onclick() {
                             ++state.cols.count;
                         }
-                    }, ">")
+                    }, "+")
                 )
             ),
 
