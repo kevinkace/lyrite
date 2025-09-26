995fac270fa8a0f30e7c14d2cd3d87a0e16f9b19
Kevin Cameron <kevinacameron@hotmail.com>
2025-09-25 21:30:16 -0700

    split lyrics

--- src/components/song/EditSong.tsx
+++ src/components/song/EditSong.tsx
@@ -1,18 +1,26 @@
 "use client";
 
+import { Card, Flex } from "@radix-ui/themes";
+
 import { useSong } from "@/contexts/SongContext";
 
-import css from "./page.module.css";
+import css from "./EditSong.module.css";
 
 export default function EditSong() {
-  const { song, loading } = useSong();
+    const { song, loading } = useSong();
 
-  if (loading) return <p>Loading…</p>;
-  if (!song) return <p>Song not found</p>;
+    if (loading) return <p>Loading…</p>;
+    if (!song) return <p>Song not found</p>;
 
-  return (
-    <>
-      <pre className={css.lyrics}>{song.lyrics}</pre>
-    </>
-  );
-}
+    const split = song.lyrics.split("\n\n");
+
+    return (
+        <Flex gap="4" direction="column" className={css.container}>
+            {split.map((line, index) => (
+                <Card key={index} variant="surface">
+                    <pre className={css.lyric} contentEditable>{line}</pre>
+                </Card>
+            ))}
+        </Flex>
+    );
+};
