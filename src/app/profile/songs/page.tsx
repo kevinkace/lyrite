81771bb78b4d639fe902a4b04bbf807a934afb03
Kevin Cameron <kevinacameron@hotmail.com>
2025-12-22 20:43:14 -0800

    whitespace

--- src/app/profile/songs/page.tsx
+++ src/app/profile/songs/page.tsx
@@ -11,17 +11,17 @@ const pageSize = 10;
 
 
 export default function ProfilePage() {
-  const { user } = useAuth();
-  const searchParams = useSearchParams();
+    const { user } = useAuth();
+    const searchParams = useSearchParams();
 
-  const page = parseInt(searchParams.get("page") || "1", 10);
-  const search = searchParams.get("search") || "";
+    const page = parseInt(searchParams.get("page") || "1", 10);
+    const search = searchParams.get("search") || "";
 
-  if (!user) return null;
+    if (!user) return null;
 
-  return (
-    <SongsProvider userId={user?.id} page={page} search={search} pageSize={pageSize}>
-      <SongsTable editControls={true} />
-    </SongsProvider>
-  );
+    return (
+        <SongsProvider userId={user?.id} page={page} search={search} pageSize={pageSize}>
+            <SongsTable editControls={true} />
+        </SongsProvider>
+    );
 }
