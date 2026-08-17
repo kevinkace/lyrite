78a04c0071e6570991d6dad8eef968598f83b108
Kevin Cameron <kevinacameron@hotmail.com>
2026-08-16 20:47:17 -0700

    types

--- src/components/layout/Article.tsx
+++ src/components/layout/Article.tsx
@@ -1,6 +1,6 @@
 import css from "./Article.module.css";
 
-export default function Article({ children }) {
+export default function Article({ children }: { children: React.ReactNode }) {
     return <article className={css.article}>
         {children}
     </article>
