9ab5d62f03fa470babdf96cb09b034d004f40b8e
Kevin Cameron <kevinacameron@hotmail.com>
2026-09-20 12:17:43 -0700

    release notes

--- src/data/releases/index.ts
+++ src/data/releases/index.ts
@@ -15,6 +15,17 @@ export type Release = {
 
 export const releases: Release[] = [
     {
+        version: "2.5.0",
+        date: "2026-09-20",
+        title: "Table column sort",
+        changes: [
+            {
+                type: "feature",
+                title: "Sort table columns"
+            },
+        ]
+    },
+    {
         version: "2.4.4",
         date: "2026-09-19",
         title: "Table items count",
